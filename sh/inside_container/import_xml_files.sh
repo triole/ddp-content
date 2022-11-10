bcf03a440fd8b206b7e219f62afcc055a36eb7a3
olaf michaelis <o.mic@web.de>
2022-11-10 13:50:55 +0100

    Refactor import mess

--- sh/inside_container/import_xml_files.sh
+++ sh/inside_container/import_xml_files.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 basedir=$(echo "${scriptdir}" | grep -Po ".*(?=/sh)")
+
+appdir="/vol/rdmo-app/"
 datadir="${basedir}/xml"
-impcmd="/vol/rdmo-app/manage.py import"
+
+pargs=()
+for val in "$@"; do
+    if [[ "${val}" =~ ^xml:.*$ ]]; then
+        datadir="$(echo "${val}" | grep -Po "(?<=xml:).*")"
+    fi
+    if [[ "${val}" =~ ^app:.*$ ]]; then
+        appdir="$(echo "${val}" | grep -Po "(?<=app:).*")"
+    fi
+done
+
+function nodir() {
+    if [[ ! -d "${1}" ]]; then
+        echo "dir does not exist: ${1}"
+        exit 1
+    fi
+}
+
+function rcmd() {
+    cmd=${@}
+    echo -e "\\033[0;93m${cmd}\\033[0m"
+    eval ${@}
+}
 
 function imp() {
     xml=$(find "${datadir}" | grep "${1}.xml" | sort | head -n 1)
-    cmd="${impcmd} ${xml}"
-    echo "${cmd}"
-    eval "${cmd}"
+    rcmd "python ${appdir}/manage.py import ${xml}"
 }
+
+nodir "${appdir}"
+nodir "${datadir}"
 
 imp attributes
 imp optionsets
@@ -17,3 +42,4 @@ imp conditions
 imp tasks
 imp catalogs
 imp views
+imp conditions
--- sh/push.sh
+++ sh/push.sh
@@ -5,31 +5,10 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 basedir=$(echo "${scriptdir}" | grep -Po ".*(?=/)")
 
-function import() {
-    echo "<yellow>Import ${1}</yellow>" | tml
-    docker exec -it ${conf_docker_name} \
-        python "${conf_manage_py}" import \
-        /tmp/data/${1}.xml
-}
+docker cp "${basedir}" ${conf_docker_name}:/tmp/
 
-# main
-docker cp "${target_dir}" ${conf_docker_name}:/tmp/
-
-if [[ "${conf_docker_name}" == "ddp-rdmo" ]]; then
-    docker cp "${basedir}/docker/import.sh" "${conf_docker_name}:/tmp/data/"
-    docker cp "${basedir}/docker/pg.sh" "${conf_docker_name}:/tmp/data/"
-
-    t=$(mktemp)
-    chmod 777 "${t}"
-    echo "${conf_manage_py}" >"${t}"
-    docker cp "${t}" "${conf_docker_name}:/tmp/data/pm.txt"
-
-    # docker exec "${conf_docker_name}" "/tmp/data/import.sh"
-else
-    import attributes
-    import conditions
-    import optionsets
-    import tasks
-    import catalogs
-    import views
-fi
+# import script can take: xml folder and rdmo app folder
+docker exec ${conf_docker_name} \
+    /tmp/ddp-content/sh/inside_container/import_xml_files.sh \
+    "xml:/tmp/ddp-content/xml/data" \
+    "app:/home/rdmo/rdmo-app"
