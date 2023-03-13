21ef8836210e54637c4c4b8ef93393a9da90e42b
olaf michaelis <o.mic@web.de>
2023-03-13 10:23:26 +0100

    Add one more warning

--- sh/inside_container/clear_and_import.sh
+++ sh/inside_container/clear_and_import.sh
@@ -3,5 +3,11 @@
 scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 scriptname="$(basename "${0}")"
 
+if [[ -z $(which psql) ]]; then
+    echo -e "\n[error] psql command line tools required, consider installing..."
+    echo -e "\n  apt install postgresql-client\n"
+    exit 1
+fi
+
 find "${scriptdir}" -regex ".*\.sh$" | grep -v "${scriptname}" |
     sort | xargs -i bash {}
