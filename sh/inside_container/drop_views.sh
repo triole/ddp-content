4601374590b1bd8be6d8e07778a410f692c55117
olaf michaelis <o.mic@web.de>
2023-03-13 10:21:51 +0100

    Add warning if psql is missing

--- sh/inside_container/drop_views.sh
+++ sh/inside_container/drop_views.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 IFS=$'\n'
+
+if [[ -z $(which psql) ]]; then
+    echo -e "\n[error] psql command line tools required, consider installing..."
+    echo -e "\n  apt install postgresql-client\n"
+    exit 1
+fi
+
 function query() {
     PGPASSWORD=${POSTGRES_PASSWORD} \
         psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} \
